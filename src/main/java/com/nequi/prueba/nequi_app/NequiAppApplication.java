adcc1d15243a23a25df4f4f7b1ed7bcd57e2d28d
Steven <srbotero34@gmail.com>
2025-02-26 15:57:23 -0500

    Commit inicial

---
+++ src/main/java/com/nequi/prueba/nequi_app/NequiAppApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.prueba.nequi_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NequiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NequiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nequi/prueba/nequi_app/NequiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.prueba.nequi_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NequiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
