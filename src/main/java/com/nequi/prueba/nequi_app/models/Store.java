7bce345b75933d83cf7e09fd2776c8ce6b39a6fe
Steven <srbotero34@gmail.com>
2025-02-26 18:27:18 -0500

    Creación del modelo Sucursal con sus atributos

---
+++ src/main/java/com/nequi/prueba/nequi_app/models/Store.java
@@ -0,0 +1,36 @@
+package com.nequi.prueba.nequi_app.models;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Document(collection = "stores")
+public class Store {
+
+    @Id
+    @NotNull(message = "Id cannot be null")
+    private String id;
+
+    @NotBlank(message = "Store name cannot be null")
+    @Size(min = 2, max = 50, message = "Store name must be between 2 and 50 characters")
+    @Indexed(unique = true)
+    private String storeName;
+
+    @NotNull(message = "Product list cannot be null")
+    @Size(min = 1, message = "There must be at least 1 product in the list")
+    @Valid
+    private List<Product> productList;
+}
