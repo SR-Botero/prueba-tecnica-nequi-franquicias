d6cbed8fc42567e23c53f810bd38a409626c5092
Steven <srbotero34@gmail.com>
2025-02-26 19:21:18 -0500

    Se empiezan a agregar los respectivos DTO, en este caso, producto

---
+++ src/main/java/com/nequi/prueba/nequi_app/dtos/ProductDTO.java
@@ -0,0 +1,29 @@
+package com.nequi.prueba.nequi_app.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ProductDTO {
+
+    @NotNull(message = "Id cannot be null")
+    private String idDTO;
+
+    @NotBlank(message = "Product name cannot be null")
+    @Size(min = 2, max = 50, message = "Product name must be between 2 and 50 characters")
+    @Indexed(unique = true)
+    private String productNameDTO;
+
+    @PositiveOrZero(message = "Stock cannot be negative")
+    private int stockDTO;
+}
