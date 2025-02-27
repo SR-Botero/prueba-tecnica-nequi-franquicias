a7893fd4672290875f9e3831ea84103570b59c37
Steven <srbotero34@gmail.com>
2025-02-26 21:48:38 -0500

    Se empiezan a crear los servicios. El primero, Producto

---
+++ src/main/java/com/nequi/prueba/nequi_app/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.nequi.prueba.nequi_app.services;
+
+import com.nequi.prueba.nequi_app.dtos.NewNameDTO;
+import com.nequi.prueba.nequi_app.dtos.NewStockDTO;
+import com.nequi.prueba.nequi_app.dtos.ProductDTO;
+import com.nequi.prueba.nequi_app.models.Product;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductService {
+
+    Mono<Product> findProductById(String productId);
+
+    Flux<Product> findAllProducts();
+
+    Mono<ProductDTO> saveProduct(ProductDTO productDTO);
+
+    Mono<Product> modifyProductStock(String productId, NewStockDTO newStockDTO);
+
+    Mono<Product> updateProductName(String productId, NewNameDTO newNameDTO);
+
+    Mono<Void> deleteProductById(String productId);
+}
