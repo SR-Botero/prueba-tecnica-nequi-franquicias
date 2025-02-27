42129747d99d9a1e92d70bdf01b284473ad8283d
Steven <srbotero34@gmail.com>
2025-02-26 21:51:27 -0500

    Se crea el servicio de Franquicia

---
+++ src/main/java/com/nequi/prueba/nequi_app/services/FranchiseService.java
@@ -0,0 +1,28 @@
+package com.nequi.prueba.nequi_app.services;
+
+import com.nequi.prueba.nequi_app.dtos.FranchiseDTO;
+import com.nequi.prueba.nequi_app.dtos.NewNameDTO;
+import com.nequi.prueba.nequi_app.dtos.ProductWithMostStockDTO;
+import com.nequi.prueba.nequi_app.dtos.StoreDTO;
+import com.nequi.prueba.nequi_app.models.Franchise;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public interface FranchiseService {
+
+    Mono<Franchise> findFranchiseById(String franchiseId);
+
+    Flux<Franchise> findAllFranchises();
+
+    Mono<List<ProductWithMostStockDTO>>findProductWithMaxStockByStore(String franchiseId);
+
+    Mono<Franchise> saveFranchise(FranchiseDTO franchiseDTO);
+
+    Mono<Franchise> saveStoreByFranchise(String franchiseId, StoreDTO storeDTO);
+
+    Mono<Franchise> updateFranchiseName(String franchiseId, NewNameDTO newNameDTO);
+
+    Mono<Void> deleteFranchiseById(String franchiseId);
+}
