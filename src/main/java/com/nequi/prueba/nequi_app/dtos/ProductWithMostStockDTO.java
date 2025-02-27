58923e42ba8153ebe0433cfda12c2412d2783355
Steven <srbotero34@gmail.com>
2025-02-26 20:48:35 -0500

    Configuración de MapStruct en el pom.xml

--- src/main/java/com/nequi/prueba/nequi_app/dtos/ProductWithMostStockDTO.java
+++ src/main/java/com/nequi/prueba/nequi_app/dtos/ProductWithMostStockDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class ProductWithMostStock {
+public class ProductWithMostStockDTO {
 
     private String storeNameDTO;
 
