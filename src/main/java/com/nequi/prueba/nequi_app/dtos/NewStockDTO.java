9dc943d971d43a119d58adc62458960f8cfa228e
Steven <srbotero34@gmail.com>
2025-02-26 20:33:53 -0500

    Se agrega el DTO para el nuevo stock

---
+++ src/main/java/com/nequi/prueba/nequi_app/dtos/NewStockDTO.java
@@ -0,0 +1,15 @@
+package com.nequi.prueba.nequi_app.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class NewStockDTO {
+
+    private int newStockDTO;
+}
